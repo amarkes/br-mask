285bc976b5562916bd38b7e1a14fb9a4bb895094
Anderson Pereira <anderson.pereira@insidesistemas.com.br>
2023-06-09 17:33:05 -0300

    fix: fixed code in new versions of ionic and angular

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,22 @@
-import { CommonModule } from '@angular/common';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import {
+  NgModule,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ModuleWithProviders,
+} from "@angular/core";
 
-import { BrMaskDirective } from './directives/br-mask';
-
+import { BrMaskDirective } from "./directives/br-mask";
 
 @NgModule({
-  declarations: [
-    BrMaskDirective
-  ],
-  exports: [
-    BrMaskDirective
-  ],
-  imports: [
-    CommonModule
-  ],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ]
+  declarations: [BrMaskDirective],
+  exports: [BrMaskDirective],
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class BrMaskerModule {}
+export class BrMaskerModule {
+  static forRoot(): ModuleWithProviders<any> {
+    return {
+      ngModule: BrMaskDirective,
+    };
+  }
+}
